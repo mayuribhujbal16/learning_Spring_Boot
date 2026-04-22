cfd515fcd63fdade2d4e41f808e5a9a72a837115
mayuribhujbal16 <mayuribhujbal9893@gmail.com>
2026-04-22 18:09:53 +0530

    Learned how to solve Ambiguity Issues using 3 args.

---
+++ AmbiguityIssueTest/src/com/model/Person.java
@@ -0,0 +1,24 @@
+package com.model;
+
+public class Person {
+
+	public Person(String name, int id) 
+	{
+		System.out.println("Constructor Executeddd");
+
+	}
+	
+	public Person(double n2, double i2) 
+	{
+		System.out.println("Constructor of Double Executeddd");
+
+	}
+	
+	public Person(String city , String country) 
+	{
+		System.out.println("Constructor of String Executeddd");
+
+	}
+	
+
+}
